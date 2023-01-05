2da27942055762591e4445085ed25972ae947185
ducdv79 <duc.dv@beetechsoft.com>
2023-01-05 18:34:58 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
-import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
@@ -19,13 +18,6 @@ var routes = [
         name: "Icons",
         icon: "ni ni-planet text-blue",
         element: <Icons />,
-        layout: "/admin"
-    },
-    {
-        path: "/maps",
-        name: "Maps",
-        icon: "ni ni-pin-3 text-orange",
-        element: <Maps />,
         layout: "/admin"
     },
     {
